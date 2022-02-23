bc2e38bd30ee174eaa84b9382e4af7703055c73b
Pratik <pratikganjale55@gmail.com>
2022-02-23 18:03:22 +0530

    featured.js

---
+++ featured.js
@@ -0,0 +1,556 @@
+var featuredData = [
+    {
+        image_url:"https://digitaleat.kfc.com/menus/image/bare/kfc-8pcBucketMealStandard?q=75",
+        name: "8 pc. Meal",
+        calories : "calories: 2300-4980" ,
+        location : "Set location for pricing" ,
+        
+    },
+    {
+        image_url:"https://digitaleat.kfc.com/menus/image/bare/kfc-sideslovers3sides?q=75",
+        name: "Side lovers Meal",
+        calories : "calories: 2850-2900" ,
+        location : "Set location for pricing" ,
+    },
+    {
+        image_url:"https://digitaleat.kfc.com/menus/image/bare/kfc-sandwichandtendersmeal?q=75",
+        name: "Sandwishes & Tender Meal",
+        calories : "calories: 3800-4000" ,
+        location : "Set location for pricing" ,
+    },
+    {
+        image_url:"https://digitaleat.kfc.com/menus/image/bare/kfc-sandwichandsidesmeal?q=75",
+        name: "Sandwishes & Sides",
+        calories : "calories: 3040-4200" ,
+        location : "Set location for pricing" ,
+    },
+    {
+        image_url:"https://digitaleat.kfc.com/menus/image/bare/kfc-8pcTendersMeal?q=75",
+        name: "8 Pics Tender Meal",
+        calories : "calories: 3040-3800" ,
+        location : "Set location for pricing" ,
+    },
+    {
+        image_url:"https://digitaleat.kfc.com/menus/image/bare/kfc-cknsndcombo?q=75",
+        name: "KFC Chicken Size Combo",
+        calories : "calories: 940-1160" ,
+        location : "Set location for pricing" ,
+    },
+    {
+        image_url:"https://digitaleat.kfc.com/menus/image/bare/kfc-10pcfeast?q=75",
+        name: "10 Pieace Feats",
+        calories : "calories: 4140-4167" ,
+        location : "Set location for pricing" ,
+    },
+    {
+        image_url:"https://digitaleat.kfc.com/menus/image/bare/kfc-FamousBowlFillUp?q=75",
+        name: "Famous Bowl Fill up",
+        calories : "calories: 840-4120" ,
+        location : "Set location for pricing" ,
+    },
+    {
+        image_url:"https://digitaleat.kfc.com/menus/image/bare/kfc-3pcBox2B1W?q=75",
+        name: "3 Pics Chiken Box - 1 Wing",
+        calories : "calories: 840-2150" ,
+        location : "Set location for pricing" ,
+    },
+    {
+        image_url:"https://digitaleat.kfc.com/menus/image/bare/kfc-5TenderCombo?q=75",
+        name: "5 Pics Tenders Combo",
+        calories : "calories: 930-1610" ,
+        location : "Set location for pricing" ,
+    },
+    {
+        image_url:"https://digitaleat.kfc.com/menus/image/bare/kfc-8pcBucket&8TendersBucket?q=75",
+        name: "8 pic Chiken + 8 Tender Fill up",
+        calories : "calories: 5200-6260" ,
+        location : "Set location for pricing" ,
+    },
+]
+
+localStorage.setItem("featuredData", JSON.stringify(featuredData));
+
+var bucketData = [
+    {
+        image_url:"https://digitaleat.kfc.com/menus/image/bare/kfc-8pcBucketMealStandard?q=75",
+        name: "8 pc. Meal",
+        calories : "calories: 2300-4980" ,
+        location : "Set location for pricing" ,
+        
+    },
+    {
+        image_url:"https://digitaleat.kfc.com/menus/image/bare/kfc-sideslovers3sides?q=75",
+        name: "Side lovers Meal",
+        calories : "calories: 2850-2900" ,
+        location : "Set location for pricing" ,
+        
+    },
+    {
+        image_url:"https://digitaleat.kfc.com/menus/image/bare/kfc-8pcFamilyFillUpDark?q=75",
+        name: "Sandwishes & Tender Meal",
+        calories : "calories: 3800-4000" ,
+        location : "Set location for pricing" ,
+        
+    },
+    {
+        image_url:"https://digitaleat.kfc.com/menus/image/bare/kfc-8pcFamilyFillUpStandard?q=75",
+        name: "Sandwishes & Tender Meal",
+        calories : "calories: 3800-4000" ,
+        location : "Set location for pricing" ,
+    },
+    {
+        image_url:"https://digitaleat.kfc.com/menus/image/bare/kfc-8pcBucketStandard?q=75",
+        name: "KFC Chicken Size Combo",
+        calories : "calories: 3040-3800" ,
+        location : "Set location for pricing" ,
+    },
+    {
+        image_url:"https://digitaleat.kfc.com/menus/image/bare/kfc-10pcfeast?q=75",
+        name: "10 piece Feast",
+        calories : "calories: 900-2150" ,
+        location : "Set location for pricing" ,
+    },
+    {
+        image_url:"https://digitaleat.kfc.com/menus/image/bare/kfc-12pcBucketMealStandard?q=75",
+        name: "12 piece Meal",
+        calories : "calories: 900-2150" ,
+        location : "Set location for pricing" ,
+    },
+    {
+        image_url:"https://digitaleat.kfc.com/menus/image/bare/kfc-16pcBucketMealStandard?q=75",
+        name: "16 piece Meal",
+        calories : "calories: 1300-2150" ,
+        location : "Set location for pricing" ,
+    },
+]
+localStorage.setItem("bucketData", JSON.stringify(bucketData));
+
+var tendersData = [
+    {
+        image_url:"https://digitaleat.kfc.com/menus/image/bare/kfc-8pcTendersMeal?q=75",
+        name: "8 pc. Meal",
+        calories : "calories: 2300-4980" ,
+        location : "Set location for pricing" ,
+        
+    },
+    {
+        image_url:"https://digitaleat.kfc.com/menus/image/bare/kfc-TendersCombo?q=75",
+        name: "Sandwishes & Tender Meal",
+        calories : "calories: 2300-4980" ,
+        location : "Set location for pricing" ,
+        
+    },
+    {
+        image_url:"https://digitaleat.kfc.com/menus/image/bare/kfc-4TendersCombo?q=75",
+        name: "8 pc. Meal",
+        calories : "calories: 2380-4080" ,
+        location : "Set location for pricing" ,
+        
+    },
+    {
+        image_url:"https://digitaleat.kfc.com/menus/image/bare/kfc-5pcTendersBox?q=75",
+        name: "16 pc. Meal",
+        calories : "calories: 2310-3080" ,
+        location : "Set location for pricing" ,
+        
+    },
+    {
+        image_url:"https://digitaleat.kfc.com/menus/image/bare/kfc-3pcNHTendersCombo?q=75",
+        name: "10 pc. Meal",
+        calories : "calories: 800-3080" ,
+        location : "Set location for pricing" ,
+        
+    },
+    {
+        image_url:"https://digitaleat.kfc.com/menus/image/bare/kfc-new8pcTenders?q=75",
+        name: "12 pc. Meal",
+        calories : "calories: 1200-3280" ,
+        location : "Set location for pricing" ,
+        
+    },
+    {
+        image_url:"https://digitaleat.kfc.com/menus/image/bare/kfc-new16pcTendersMeal?q=75",
+        name: "8 pc. Meal",
+        calories : "calories: 2200-4980" ,
+        location : "Set location for pricing" ,
+        
+    },
+    {
+        image_url:"https://digitaleat.kfc.com/menus/image/bare/kfc-new12pcTenders?q=75",
+        name: "Side lovers Meal",
+        calories : "calories: 900-4980" ,
+        location : "Set location for pricing" ,
+        
+    },
+]
+localStorage.setItem("tendersData", JSON.stringify(tendersData));
+
+var sandwishesData = [
+    {
+        image_url:"https://digitaleat.kfc.com/menus/image/bare/kfc-sandwichandtendersmeal?q=75",
+        name: "8 pc. Meal",
+        calories : "calories: 2300-4980" ,
+        location : "Set location for pricing" ,
+        
+    },
+    {
+        image_url:"https://digitaleat.kfc.com/menus/image/bare/kfc-cknsndcombo?q=75",
+        name: "Sandwishes & Tender Meal",
+        calories : "calories: 2300-4980" ,
+        location : "Set location for pricing" ,
+        
+    },
+    {
+        image_url:"https://digitaleat.kfc.com/menus/image/bare/kfc-cknsndbigbox?q=75",
+        name: "8 pc. Meal",
+        calories : "calories: 2380-4080" ,
+        location : "Set location for pricing" ,
+        
+    },
+    {
+        image_url:"https://digitaleat.kfc.com/menus/image/bare/kfc-cknsndalc?q=75",
+        name: "16 pc. Meal",
+        calories : "calories: 2310-3080" ,
+        location : "Set location for pricing" ,
+        
+    },
+    {
+        image_url:"https://digitaleat.kfc.com/menus/image/bare/kfc-AlcChLtl?q=75",
+        name: "10 pc. Meal",
+        calories : "calories: 800-3080" ,
+        location : "Set location for pricing" ,
+        
+    },
+    {
+        image_url:"https://digitaleat.kfc.com/menus/image/bare/kfc-ChLtlCombo?q=75",
+        name: "12 pc. Meal",
+        calories : "calories: 1200-3280" ,
+        location : "Set location for pricing" ,
+        
+    },
+    {
+        image_url:"https://digitaleat.kfc.com/menus/image/bare/kfc-ChLtlCombo?q=75",
+        name: "8 pc. Meal",
+        calories : "calories: 2200-4980" ,
+        location : "Set location for pricing" ,
+        
+    },
+    
+]
+localStorage.setItem("sandwishesData", JSON.stringify(sandwishesData));
+
+var friedchickenData = [
+    {
+        image_url:"https://digitaleat.kfc.com/menus/image/bare/kfc-1BrFU?q=75",
+        name: "8 pc. Meal",
+        calories : "calories: 2300-4980" ,
+        location : "Set location for pricing" ,
+        
+    },
+    {
+        image_url:"https://digitaleat.kfc.com/menus/image/bare/kfc-1BrFU?q=75",
+        name: "Kentucky fried Window",
+        calories : "calories: 2300-4980" ,
+        location : "Set location for pricing" ,
+        
+    },
+    {
+        image_url:"https://digitaleat.kfc.com/menus/image/bare/kfc-3pcCombo1B1T1W?q=75",
+        name: "8 Kentucky fried Window",
+        calories : "calories: 2380-4080" ,
+        location : "Set location for pricing" ,
+        
+    },
+    {
+        image_url:"https://digitaleat.kfc.com/menus/image/bare/kfc-3pcCombo1B1T1W?q=75",
+        name: "16 pc. Meal",
+        calories : "calories: 2310-3080" ,
+        location : "Set location for pricing" ,
+        
+    },
+    {
+        image_url:"https://digitaleat.kfc.com/menus/image/bare/kfc-3pcCombo1B1T1W?q=75",
+        name: "10 Kentucky fried Window",
+        calories : "calories: 800-3080" ,
+        location : "Set location for pricing" ,
+        
+    },
+    {
+        image_url:"https://digitaleat.kfc.com/menus/image/bare/kfc-4pcCombo?q=75",
+        name: "12 Kentucky fried Window",
+        calories : "calories: 1200-3280" ,
+        location : "Set location for pricing" ,
+        
+    },
+    {
+        image_url:"https://digitaleat.kfc.com/menus/image/bare/kfc-6WingsUnsauced?q=75",
+        name: "8 pc. Meal",
+        calories : "calories: 2200-4980" ,
+        location : "Set location for pricing" ,
+        
+    },
+    {
+        image_url:"https://digitaleat.kfc.com/menus/image/bare/kfc-6WingsUnsauced?q=75",
+        name: "8 Kentucky fried Window",
+        calories : "calories: 2200-4980" ,
+        location : "Set location for pricing" ,
+        
+    },
+    {
+        image_url:"https://digitaleat.kfc.com/menus/image/bare/kfc-24WingsUnsauced?q=75",
+        name: "Kentucky fried Window",
+        calories : "calories: 2200-4980" ,
+        location : "Set location for pricing" ,
+        
+    },
+]
+localStorage.setItem("friedchickenData", JSON.stringify(friedchickenData));
+
+var potpiesData = [
+    {
+        image_url:"https://digitaleat.kfc.com/menus/image/bare/kfc-FamousBowlFillUp?q=75",
+        name: "Famous Bowl Fill Up",
+        calories : "calories: 2300-4980" ,
+        location : "Set location for pricing" ,
+        
+    },
+    {
+        image_url:"https://digitaleat.kfc.com/menus/image/bare/kfc-PotPieFillUp?q=75",
+        name: "Pot Pie Fill Up",
+        calories : "calories: 900-3000" ,
+        location : "Set location for pricing" ,
+        
+    },
+]
+localStorage.setItem("potpiesData", JSON.stringify(potpiesData));
+
+var alacarteData = [
+    {
+        image_url:"https://digitaleat.kfc.com/menus/image/bare/kfc-AlcBreast?q=75",
+        name: "A La Catry Wing",
+        calories : "calories: 2300-4980" ,
+        location : "Set location for pricing" ,
+        
+    },
+    {
+        image_url:"https://digitaleat.kfc.com/menus/image/bare/kfc-AlcLeg?q=75",
+        name: "Pot Pie Thongh",
+        calories : "calories: 900-3000" ,
+        location : "Set location for pricing" ,
+        
+    },
+    {
+        image_url:"https://digitaleat.kfc.com/menus/image/bare/kfc-AlcThigh?q=75",
+        name: "Pot Pie Fill Up",
+        calories : "calories: 900-3000" ,
+        location : "Set location for pricing" ,
+        
+    },
+    {
+        image_url:"https://digitaleat.kfc.com/menus/image/bare/kfc-AlcWing?q=75",
+        name: "Famous Bowl",
+        calories : "calories: 900-3000" ,
+        location : "Set location for pricing" ,
+        
+    },
+    {
+        image_url:"https://digitaleat.kfc.com/menus/image/bare/kfc-FamousBowl?q=75",
+        name: "A La Catry Tender ",
+        calories : "calories: 900-3000" ,
+        location : "Set location for pricing" ,
+        
+    },
+    {
+        image_url:"https://digitaleat.kfc.com/menus/image/bare/kfc-PotPie?q=75",
+        name: "Pot Pie ",
+        calories : "calories: 900-3000" ,
+        location : "Set location for pricing" ,
+        
+    },
+]
+localStorage.setItem("alacarteData", JSON.stringify(alacarteData));
+
+var slidesData = [
+    {
+        image_url:"https://digitaleat.kfc.com/menus/image/bare/kfc-IndFries?q=75",
+        name: "Screate Receipe Fries",
+        calories : "calories: 2300-4980" ,
+        location : "Set location for pricing" ,
+        
+    },
+    {
+        image_url:"https://digitaleat.kfc.com/menus/image/bare/kfc-IndMashGravy?q=75",
+        name: "Mashed Potatos Gravy",
+        calories : "calories: 900-3000" ,
+        location : "Set location for pricing" ,
+        
+    },
+    {
+        image_url:"https://digitaleat.kfc.com/menus/image/bare/kfc-IndMacCheese?q=75",
+        name: "Mac & Cheese",
+        calories : "calories: 900-3000" ,
+        location : "Set location for pricing" ,
+        
+    },
+    {
+        image_url:"https://digitaleat.kfc.com/menus/image/bare/kfc-biscuit?q=75",
+        name: "Biscuit",
+        calories : "calories: 900-3000" ,
+        location : "Set location for pricing" ,
+        
+    },
+    {
+        image_url:"https://digitaleat.kfc.com/menus/image/bare/kfc-IndColeslaw?q=75",
+        name: "Cole Saw",
+        calories : "calories: 900-3000" ,
+        location : "Set location for pricing" ,
+        
+    },
+    {
+        image_url:"https://digitaleat.kfc.com/menus/image/bare/kfc-IndGravy?q=75",
+        name: "Gravy",
+        calories : "calories: 900-3000" ,
+        location : "Set location for pricing" ,
+        
+    },
+    {
+        image_url:"https://digitaleat.kfc.com/menus/image/bare/kfc-IndCorn?q=75",
+        name: "Whole Corn",
+        calories : "calories: 900-3000" ,
+        location : "Set location for pricing" ,
+        
+    },
+    {
+        image_url:"https://digitaleat.kfc.com/menus/image/bare/kfc-IndMash?q=75",
+        name: "Mashed Potatos",
+        calories : "calories: 900-3000" ,
+        location : "Set location for pricing" ,
+        
+    },
+]
+localStorage.setItem("slidesData", JSON.stringify(slidesData));
+
+var  saucesData = [
+    {
+        image_url:"https://digitaleat.kfc.com/menus/image/bare/kfc-KFCSauce?q=75",
+        name: "KFC sauce",
+        calories : "calories: 230" ,
+        location : "Set location for pricing" ,
+        
+    },
+    {
+        image_url:"https://digitaleat.kfc.com/menus/image/bare/kfc-HoneyBBQ?q=75",
+        name: "Honey Sauce",
+        calories : "calories: 90" ,
+        location : "Set location for pricing" ,
+        
+    },
+    {
+        image_url:"https://digitaleat.kfc.com/menus/image/bare/kfc-HoneyMustard?q=75",
+        name: "Honey Mosterd",
+        calories : "calories: 40" ,
+        location : "Set location for pricing" ,
+        
+    },
+    {
+        image_url:"https://digitaleat.kfc.com/menus/image/bare/kfc-Ranch?q=75",
+        name: "Classic Ranch",
+        calories : "calories: 60" ,
+        location : "Set location for pricing" ,
+        
+    },
+    {
+        image_url:"https://digitaleat.kfc.com/menus/image/bare/kfc-honeySauce2?q=75",
+        name: "Honey Sauce",
+        calories : "calories: 50" ,
+        location : "Set location for pricing" ,
+        
+    },
+    {
+        image_url:"https://digitaleat.kfc.com/menus/image/bare/kfc-hotSauce2?q=75",
+        name: "Hot Suace",
+        calories : "calories: 40" ,
+        location : "Set location for pricing" ,
+        
+    },
+    {
+        image_url:"https://digitaleat.kfc.com/menus/image/bare/kfc-ketchup2?q=75",
+        name: "Ketchup",
+        calories : "calories: 60" ,
+        location : "Set location for pricing" ,
+        
+    },
+   
+]
+localStorage.setItem("saucesData", JSON.stringify(saucesData));
+
+var desertsData = [
+    {
+        image_url:"https://digitaleat.kfc.com/menus/image/bare/kfc-1Cookie?q=75",
+        name: "Chocolate Chip Cookies",
+        calories : "calories: 120" ,
+        location : "Set location for pricing" ,
+        
+    },
+    {
+        image_url:"https://images.ctfassets.net/9tka4b3550oc/1XFzu9NKNtkE8UpD3plXJ3/c8a6893c77aaeb16c82a0e2b43e2c59c/2.jpg?q=75&w=640",
+        name: "Chocolate Cake",
+        calories : "calories: 300" ,
+        location : "Set location for pricing" ,
+        
+    },
+]
+localStorage.setItem("desertsData", JSON.stringify(desertsData));
+
+var drinksData = [
+    {
+        image_url:"https://digitaleat.kfc.com/menus/image/bare/kfc-Pepsi?q=75",
+        name: "Diet Pepies",
+        calories : "calories: 120" ,
+        location : "Set location for pricing" ,
+        
+    },
+    {
+        image_url:"https://digitaleat.kfc.com/menus/image/bare/kfc-mtndew?q=75",
+        name: "Mountain Dew Sweet",
+        calories : "calories: 200" ,
+        location : "Set location for pricing" ,
+        
+    },
+    {
+        image_url:"https://digitaleat.kfc.com/menus/image/bare/kfc-mtndewsweetlightning?q=75",
+        name: "Sierra Mist",
+        calories : "calories: 300" ,
+        location : "Set location for pricing" ,
+        
+    },
+    {
+        image_url:"https://digitaleat.kfc.com/menus/image/bare/kfc-sierramist?q=75",
+        name: "Sweet Tea",
+        calories : "calories: 100" ,
+        location : "Set location for pricing" ,
+        
+    },
+    {
+        image_url:"https://digitaleat.kfc.com/menus/image/bare/kfc-sweetTea?q=75",
+        name: "Lemonade",
+        calories : "calories: 400" ,
+        location : "Set location for pricing" ,
+        
+    },
+    {
+        image_url:"https://digitaleat.kfc.com/menus/image/bare/kfc-StrLemMed?q=75",
+        name: "Strawbery Lemonon",
+        calories : "calories: 30" ,
+        location : "Set location for pricing" ,
+        
+    },
+    {
+        image_url:"https://digitaleat.kfc.com/menus/image/bare/kfc-BevBucket?q=75",
+        name: "Gallon Baverage",
+        calories : "calories: 0-900" ,
+        location : "Set location for pricing" ,
+        
+    },
+]
+localStorage.setItem("drinksData", JSON.stringify(drinksData));
